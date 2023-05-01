28b62dff34aa9295d7c1a55506158ae336677418
Evgeniy Laptev <79728932+laps78@users.noreply.github.com>
2023-05-01 13:11:30 +0300

    main page modified

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Ссылки на необходимые сервисы для настройки подключения с chatGPT:
+        </h1>
       </header>
+      <main>
+        <article>
+          <h2>VPN</h2>
+          <a href="#">Ссылка на сервис</a>
+          <p>
+            VPN требуется для обхода внутренних блокировок openai, связанных с
+            антироссийскими санкциями. Ссылка временно не работает, но это в
+            скором времени будет исправлено.
+          </p>
+        </article>
+        <article>
+          <h2>SMS-activate</h2>
+          <a href="https://sms-activate.org/?ref=6834896">Ссылка на сервис</a>
+          <p>
+            Данный сервис позволяет арендовать телефонный номер для приема смс,
+            который нужен для полноценной регистрации на openai. Перейдите по
+            ссылке, пройдите регистрацию и арендуйте номер той страны, откуда
+            идет ваш VPN-траффик.
+          </p>
+        </article>
+      </main>
     </div>
   );
 }
